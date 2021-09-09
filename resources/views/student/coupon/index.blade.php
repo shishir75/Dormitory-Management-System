80180594403b149b4230d4bf3c76ab7cb13699e0
shishir75 <shishir.srdr16@gmail.com>
2021-09-09 20:55:36 +0600

    Student Food Coupon

--- resources/views/student/coupon/index.blade.php
+++ resources/views/student/coupon/index.blade.php
@@ -30,6 +30,7 @@
                     <div class="col-md-12 text-center">
                         <p class="h4">Student Name : <span class="text-bold">{{ $student->name }}</span></p>
                         <p class="h4">Department Name : <span class="text-bold">{{ $student->dept->name }}</span></p>
+                        <p class="h4">Dining Name : <span class="text-bold">{{ $dining->username }}</span></p>
                         <p class="h4">Available Balance :
                             @if ($balance == null)
                                 <span class="text-bold my-2 badge badge-danger">0 BDT</span>
@@ -44,12 +45,52 @@
 
 
                 <div class="row">
+                    <div class="col-md-12">
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary float-right mb-3" data-toggle="modal" data-target="#purchaseCoupon">
+                            Purchase a Coupon
+                        </button>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="purchaseCoupon" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Purchase a Coupon</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                                </div>
+                                <div class="modal-body">
+                                    <form>
+                                        <div class="form-group">
+                                            <label for="exampleFormControlSelect1">Example select</label>
+                                            <select class="form-control" id="exampleFormControlSelect1">
+                                              <option>1</option>
+                                              <option>2</option>
+                                              <option>3</option>
+                                              <option>4</option>
+                                              <option>5</option>
+                                            </select>
+                                          </div>
+                                      </form>
+                                </div>
+                                <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+
+
                     <!-- left column -->
                     <div class="col-md-12">
                         <!-- general form elements -->
                         <div class="card card-info">
                             <div class="card-header">
-                                <h3 class="card-title">Trasction Details <span class="float-right badge badge-success py3">Session {{ $student->session->name }}</span></h3>
+                                <h3 class="card-title">Food Coupon Details <span class="float-right badge badge-success py3">Session {{ $student->session->name }}</span></h3>
                             </div>
                             <!-- /.card-header -->
 
@@ -71,7 +112,7 @@
                                             <td>{{ $key + 1 }}</td>
                                             <td>{{ $coupon_detail->name }}</td>
                                             <td >{{ $coupon_detail->type }}</td>
-                                            @if ($coupon_detail->is_valid === 'Valid')
+                                            @if ($coupon_detail->is_valid === 'valid')
                                                 <td class="my-2 badge badge-success">+ {{ $coupon_detail->is_valid }}</td>
                                             @else
                                                 <td class="my-2 badge badge-danger">- {{ $coupon_detail->is_valid }}</td>
