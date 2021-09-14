95fc1c208c1cbc5d27f90cdb388101da5b3ae865
shishir75 <shishir.srdr16@gmail.com>
2021-09-14 23:16:50 +0600

    Dept Office Dashboard

--- app/Http/Controllers/Dept_Office/DashboardController.php
+++ app/Http/Controllers/Dept_Office/DashboardController.php
@@ -3,21 +3,22 @@
 namespace App\Http\Controllers\Dept_Office;
 
 use App\Http\Controllers\Controller;
+use App\Models\Dept;
+use App\Models\Hall;
+use App\Models\Student;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        // $dept = Dept::where( 'name', Auth::user()->name )->first();
-        // $sessions = Session::count();
-        //$teachers = Teacher::where('dept_id', $dept->id)->count();
-        //$students = Student::where('dept_id', $dept->id)->count();
-        //$courses = Course::where('dept_id', $dept->id)->count();
-        //$externals = CourseTeacher::where('dept_id', $dept->id)->count();
-        //$year_heads = YearHead::where('dept_id', $dept->id)->count();
+        $dept = Dept::where( 'name', Auth::user()->name )->first();
+        $current_students = Student::where( 'dept_id', $dept->id )->where( 'status', 1 )->count();
+        $waiting_for_hall_approval_students = Student::where( 'dept_id', $dept->id )->where( 'status', 2 )->count();
+        $ex_students = Student::where( 'dept_id', $dept->id )->where( 'status', 3 )->count();
+        $available_seats = Hall::sum( 'available_seat' );
+        $halls = Hall::count();
 
-        //return view('dept_office.dashboard', compact('sessions',  'students', 'courses', 'externals', 'year_heads'));
-
-        return view( 'dept_office.dashboard' );
+        return view( 'dept_office.dashboard', compact( 'current_students', 'waiting_for_hall_approval_students', 'ex_students', 'available_seats', 'halls' ) );
     }
 }
--- resources/views/dept_office/dashboard.blade.php
+++ resources/views/dept_office/dashboard.blade.php
@@ -32,8 +32,8 @@
                             <span class="info-box-icon bg-info"><i class="fa fa-book"></i></span>
 
                             <div class="info-box-content">
-                                <span class="info-box-text">Courses</span>
-                                <span class="info-box-number"></span>
+                                <span class="info-box-text">Current Students</span>
+                                <span class="info-box-number">{{ $current_students }}</span>
                             </div>
                             <!-- /.info-box-content -->
                         </div>
@@ -45,8 +45,8 @@
                             <span class="info-box-icon bg-success"><i class="fa fa-clock-o"></i></span>
 
                             <div class="info-box-content">
-                                <span class="info-box-text">Sessions</span>
-                                <span class="info-box-number"></span>
+                                <span class="info-box-text">Waiting for Halls Approval Students</span>
+                                <span class="info-box-number">{{ $waiting_for_hall_approval_students }}</span>
                             </div>
                             <!-- /.info-box-content -->
                         </div>
@@ -58,8 +58,8 @@
                             <span class="info-box-icon bg-warning"><i class="fa fa-user"></i></span>
 
                             <div class="info-box-content">
-                                <span class="info-box-text">Teachers</span>
-                                <span class="info-box-number"></span>
+                                <span class="info-box-text">Ex-Students</span>
+                                <span class="info-box-number">{{ $ex_students }}</span>
                             </div>
                             <!-- /.info-box-content -->
                         </div>
@@ -71,8 +71,8 @@
                             <span class="info-box-icon bg-gray"><i class="fa fa-user-plus"></i></span>
 
                             <div class="info-box-content">
-                                <span class="info-box-text">Course Teacher</span>
-                                <span class="info-box-number"></span>
+                                <span class="info-box-text">Available Seats</span>
+                                <span class="info-box-number">{{ $available_seats }}</span>
                             </div>
                             <!-- /.info-box-content -->
                         </div>
@@ -85,8 +85,8 @@
                             <span class="info-box-icon bg-info"><i class="fa fa-users"></i></span>
 
                             <div class="info-box-content">
-                                <span class="info-box-text">Year Head</span>
-                                <span class="info-box-number"></span>
+                                <span class="info-box-text">Total Halls</span>
+                                <span class="info-box-number">{{ $halls }}</span>
                             </div>
                             <!-- /.info-box-content -->
                         </div>
@@ -99,8 +99,8 @@
                             <span class="info-box-icon bg-success"><i class="fa fa-graduation-cap"></i></span>
 
                             <div class="info-box-content">
-                                <span class="info-box-text">Students</span>
-                                <span class="info-box-number"></span>
+                                <span class="info-box-text">Total Students</span>
+                                <span class="info-box-number">{{ $current_students + $waiting_for_hall_approval_students + $ex_students }}</span>
                             </div>
                             <!-- /.info-box-content -->
                         </div>
